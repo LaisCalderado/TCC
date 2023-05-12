b094957de9347cd8f94bf67d9418f73905076c44
LaisCalderado <laiscalderaro06@gmail.com>
2023-05-11 22:35:21 -0400

    Add opção de Like e Algoritmo de Recomendação

--- src/pages/ProjectsPage/index.jsx
+++ src/pages/ProjectsPage/index.jsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import "./style.css";
 
@@ -30,8 +30,20 @@ const projects = [
   },
 ];
 
-const Project = ({ project, onDelete }) => {
-  const { title, description, views, likes, imageUrl } = project;
+const Project = ({ project, onDelete, onLike }) => {
+  const { id, title, description, views, likes, imageUrl } = project;
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+    if (liked) {
+      setLiked(false);
+      onLike(id, false); // Informa que o like está sendo removido
+    } else {
+      setLiked(true);
+      onLike(id, true); // Informa que o like está sendo adicionado
+    }
+  };
+
 
   return (
     <>
@@ -44,19 +56,29 @@ const Project = ({ project, onDelete }) => {
           <h2>{title}</h2>
           <p>{description}</p>
           <p>Views: {views}</p>
+          <div className="like-buttons">
+            <button
+              className={`like-button ${liked ? "liked" : ""}`}
+              onClick={handleLike}
+            >
+              {liked ? "Unlike" : "Like"}
+            </button>
+          </div>
           <p>Likes: {likes}</p>
-          <button onClick={onDelete}>Deletar Projeto</button>
+          <button onClick={() => onDelete(id)}>Deletar Projeto</button>
         </div>
       </div>
     </>
   );
 };
 
+
 const ProjectsPage = () => {
   const [projectsList, setProjectsList] = useState(projects);
   const [newProjectTitle, setNewProjectTitle] = useState("");
   const [newProjectDescription, setNewProjectDescription] = useState("");
   const [showForm, setShowForm] = useState(false);
+  const [recommendedProjects, setRecommendedProjects] = useState([]);
 
   const handleDeleteProject = (id) => {
     const updatedProjectsList = projectsList.filter(
@@ -64,6 +86,45 @@ const ProjectsPage = () => {
     );
     setProjectsList(updatedProjectsList);
   };
+
+  const handleLikeProject = (id, isLiked) => {
+    const updatedProjectsList = projectsList.map((project) => {
+      if (project.id === id) {
+        if (isLiked) {
+          return {
+            ...project,
+            likes: project.likes + 1,
+          };
+        } else {
+          return {
+            ...project,
+            likes: project.likes - 1,
+          };
+        }
+      }
+      return project;
+    });
+    setProjectsList(updatedProjectsList);
+  };
+
+  const handleDislikeProject = (id) => {
+    const updatedProjectsList = projectsList.map((project) => {
+      if (project.id === id) {
+        return {
+          ...project,
+          dislikes: project.dislikes + 1,
+        };
+      }
+      return project;
+    });
+    setProjectsList(updatedProjectsList);
+  };
+
+  useEffect(() => {
+    // Algoritmo de recomendação
+    const recommended = projectsList.filter((project) => project.likes > 0);
+    setRecommendedProjects(recommended);
+  }, [projectsList]);
 
   const handleCreateProject = (event) => {
     event.preventDefault();
@@ -74,6 +135,7 @@ const ProjectsPage = () => {
       description: newProjectDescription,
       views: 0,
       likes: 0,
+      dislikes: 0,
       imageUrl: "https://picsum.photos/200/300",
     };
 
@@ -94,7 +156,8 @@ const ProjectsPage = () => {
             <Project
               key={project.id}
               project={project}
-              onDelete={() => handleDeleteProject(project.id)}
+              onDelete={handleDeleteProject}
+              onLike={handleLikeProject}
             />
           ))}
           {!showForm && (
@@ -105,6 +168,19 @@ const ProjectsPage = () => {
               </Link>
             </div>
           )}
+        </div>
+
+        <h2>Projetos Recomendados</h2>
+        <div className="recommended-projects">
+          {recommendedProjects.map((project) => (
+            <Project
+              key={project.id}
+              project={project}
+              onDelete={handleDeleteProject}
+              onLike={handleLikeProject}
+              onDislike={handleDislikeProject}
+            />
+          ))}
         </div>
       </div>
     </>
