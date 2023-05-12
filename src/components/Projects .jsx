84b29350d114d44739356a75375b92cf31b15313
LaisCalderado <laiscalderaro06@gmail.com>
2023-05-11 22:32:44 -0400

    Add opção de Like

--- src/components/Projects .jsx
+++ src/components/Projects .jsx
@@ -1,39 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Projects.css"; // importando o CSS
 
 const Projects = () => {
-  const projects = [
+  const [projectList, setProjectList] = useState([
     {
       id: 1,
-      title: "Encontro das Lembranças",
+      title: "Atraves dos Reinos",
       imageSrc: "https://i.zst.com.br/images/os-13-melhores-jogos-de-acao-e-aventura-para-videogame-em-2019-photo193408540-44-9-3c.jpg",
-      description: "O jovem guerreiro guerreiro tem a missão de salvar seu povo ",
+      description: "Guerreiro em missão de salvar seu povo ",
       views: 20,
       likes: 10,
+      liked: false,
     },
     {
       id: 2,
       title: "Caça ao Tesouro",
       imageSrc: "https://ogimg.infoglobo.com.br/in/23001849-c2f-667/FT1086A/INFOCHPDPICT000041975439.jpg",
-      description: "Encontre os tesouros perdido e ajude seus amigos no caminho.",
+      description: "Encontre os tesouros perdido.",
       views: 30,
       likes: 20,
+      liked: false,
     },
     {
       id: 3,
       title: "Corrida maluca",
       imageSrc: "https://gametimes.com.br/wp-content/uploads/2022/05/Jogos-de-Plataforma.jpg",
-      description: "Dê um up no seu carro e venha ganhar varios prêmios.",
+      description: "Ganha quem for mais agil.",
       views: 40,
       likes: 30,
-    },{
+      liked: false,
+    },
+    {
       id: 4,
       title: "Jornada dos destemidos",
       imageSrc: "https://1.bp.blogspot.com/-0iJQPQerJjE/YL-jvR9dlYI/AAAAAAAAEis/VtlvZjQOP9ETAVmnG4O4j1B43PJGaLExwCLcBGAsYHQ/s1920/frogun.png",
       description: "Destrua os inimigos e fique mais forte.",
       views: 20,
       likes: 10,
+      liked: false,
     },
     {
       id: 5,
@@ -42,6 +47,7 @@ const Projects = () => {
       description: "Voar não é o limite.",
       views: 30,
       likes: 20,
+      liked: false,
     },
     {
       id: 6,
@@ -50,14 +56,29 @@ const Projects = () => {
       description: "Aprenda novas magias e sinta-se livre",
       views: 40,
       likes: 30,
+      liked: false,
     },
-  ];
+  ]);
+
+  const handleLike = (projectId) => {
+    setProjectList((prevProjects) => {
+      return prevProjects.map((project) => {
+        if (project.id === projectId) {
+          return {
+            ...project,
+            liked: !project.liked,
+            likes: project.liked ? project.likes - 1 : project.likes + 1,
+          };
+        }
+        return project;
+      });
+    });
+  };
 
   return (
     <div className="projects">
-      <h2>Projects</h2>
       <div className="project-cards">
-        {projects.map((project) => (
+        {projectList.map((project) => (
           <div className="project-card" key={project.id}>
             <img src={project.imageSrc} alt={project.title} />
             <div className="project-details">
@@ -67,7 +88,17 @@ const Projects = () => {
                 <p>Views: {project.views}</p>
                 <p>Likes: {project.likes}</p>
               </div>
-              <Link to={project.route}>View Project</Link>
+              <div className="project-actions">
+                <Link to={project.route} className="view-button">
+                  Visualizar Projeto
+                </Link>
+                <button
+                  className={`like-button ${project.liked ? "liked" : ""}`}
+                  onClick={() => handleLike(project.id)}
+                >
+                  {project.liked ? "Unlike" : "Like"}
+                </button>
+              </div>
             </div>
           </div>
         ))}
