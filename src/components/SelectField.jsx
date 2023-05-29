d4c539107aeec86e0cda09bcee5606048cb07932
LaisCalderado <laiscalderaro06@gmail.com>
2023-05-29 15:28:23 -0400

    Add SelectField

--- src/components/SelectField.jsx
+++ src/components/SelectField.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-const SelectField = ({ label, value, onChange, options }) => {
+const SelectField = ({ label, value, onChange, options, onDataChange }) => {
+    const handleSelectChange = (event) => {
+        const selectedValue = event.target.value;
+        onChange(selectedValue);
+
+        // Verifica se onDataChange foi fornecido como prop e chama-o com os dados selecionados
+        if (onDataChange) {
+            const selectedOption = options.find((option) => option.value === selectedValue);
+            onDataChange(selectedOption);
+        }
+    };
     return (
         <div className="form-group">
             <label>{label}</label>
