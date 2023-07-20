0b25e2b3734f3d75d997f08a4acf2f6df8e38844
LaisCalderado <laiscalderaro06@gmail.com>
2023-07-19 23:30:29 -0400

    Método de criar usuário na API

--- projeto_api/config/urls.py
+++ projeto_api/config/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
-route = routers.DefaultRouter()
 from GdsSystem.api import viewsets as ProjetosViewsets
+from .views import UserCreateView
 
-route.register(r'projetos', ProjetosViewsets.ProjetosViewset , basename="Projetos")
+route = routers.DefaultRouter()
+route.register(r'projetos', ProjetosViewsets.ProjetosViewset, basename="Projetos")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls)),
-    
+    path('users/create/', UserCreateView.as_view(), name="user_create"),
 ]
