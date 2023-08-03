82a63db72ff26b4f4053142d2e09d9f3831da51d
LaisCalderado <laiscalderaro06@gmail.com>
2023-08-02 22:20:32 -0400

    Pegando as Props do Pai NewProjectPage

--- src/path/ConteudoAplicado/index.jsx
+++ src/path/ConteudoAplicado/index.jsx
@@ -3,71 +3,42 @@ import SelectField from "../../components/SelectField";
 import "./style.css"; 
 
 const ConteudoAplicado = (props) => {
-  const [selectedGrau, setSelectedGrau] = useState("");
-  const [selectedSerie, setSelectedSerie] = useState("");
-  const [selectedDisciplina, setSelectedDisciplina] = useState("");
 
   const handleGrauChange = (event) => {
-    setSelectedGrau(event.target.value);
+    props.setSelectedGrau(event.target.value);
   };
 
   const handleSerieChange = (event) => {
-    setSelectedSerie(event.target.value);
+    props.setSelectedSerie(event.target.value);
   };
 
   const handleDisciplinaChange = (event) => {
-    setSelectedDisciplina(event.target.value);
+    props.setSelectedDisciplina(event.target.value);
   };
 
-  const handleCreateProject = () => {
-    // Lógica para criar um novo projeto com as escolhas selecionadas
-    console.log("Novo projeto criado:", selectedGrau, selectedSerie, selectedDisciplina);
-  };
-
-  const saveChoicesToDatabase = () => {
-    const newChoices = {
-      selectedGrau,
-      selectedSerie,
-      selectedDisciplina,
-    };
-
-    // Converter o objeto JSON em uma string JSON
-    const jsonString = JSON.stringify(newChoices);
-
-    try {
-      // Salvar a string JSON no banco de dados
-      // (substitua essa lógica com o código adequado para o seu caso)
-      console.log("Escolhas salvas no banco de dados:", jsonString);
-    } catch (error) {
-      console.error("Erro ao salvar as escolhas no banco de dados:", error);
-    }
-  };
 
   return (
-    <div className="conteudo-aplicado-container"> {/* Adicione uma classe CSS personalizada para o container */}
+    <div className="conteudo-aplicado-container"> 
       <SelectField
         label="Qual Grau?"
-        value={selectedGrau}
+        value={props.selectedGrau}
         onChange={handleGrauChange}
         options={props.graus}
       />
 
       <SelectField
         label="Qual Série?"
-        value={selectedSerie}
+        value={props.selectedSerie}
         onChange={handleSerieChange}
         options={props.series}
       />
 
       <SelectField
         label="Qual Disciplina?"
-        value={selectedDisciplina}
+        value={props.selectedDisciplina}
         onChange={handleDisciplinaChange}
         options={props.disciplinas}
       />
-
-      <button onClick={handleCreateProject}>Criar Projeto</button>
-      <button onClick={saveChoicesToDatabase}>Salvar escolhas</button>
     </div>
   );
 };
--- src/path/Jogadores/index.jsx
+++ src/path/Jogadores/index.jsx
@@ -1,138 +1,44 @@
 import React, { useState } from "react";
 import SelectField from "../../components/SelectField";
-import { database } from "../../pages/config/firebase";
-import { ref, push, set } from "firebase/database";
 import './style.css';
 
-const Jogadores = () => {
-    const [selectedAgeRange, setSelectedAgeRange] = useState("");
-    const [selectedExperience, setSelectedExperience] = useState("");
-    const [selectedObjectives, setSelectedObjectives] = useState("");
-    const [selectedMotivation, setSelectedMotivation] = useState("");
-    const [selectedLearningStyle, setSelectedLearningStyle] = useState("");
-    const [selectedSpecialNeeds, setSelectedSpecialNeeds] = useState("");
+const Jogadores = (props) => {
 
-    const handleAgeRangeChange = (event) => {
-        setSelectedAgeRange(event.target.value);
+    const handleEstiloAprendizagemChange = (event) => {
+        props.setSelectedEstiloAprendizagem(event.target.value);
     };
 
-    const handleExperienceChange = (event) => {
-        setSelectedExperience(event.target.value);
+    const handleInteressesChange = (event) => {
+        props.setSelectedInteresses(event.target.value);
     };
 
-    const handleObjectivesChange = (event) => {
-        setSelectedObjectives(event.target.value);
-    };
-
-    const handleMotivationChange = (event) => {
-        setSelectedMotivation(event.target.value);
-    };
-
-    const handleLearningStyleChange = (event) => {
-        setSelectedLearningStyle(event.target.value);
-    };
-
-    const handleSpecialNeedsChange = (event) => {
-        setSelectedSpecialNeeds(event.target.value);
-    };
-
-    const saveChoicesToDatabase = () => {
-        const data = {
-            ageRange: selectedAgeRange,
-            experience: selectedExperience,
-            objectives: selectedObjectives,
-            motivation: selectedMotivation,
-            learningStyle: selectedLearningStyle,
-            specialNeeds: selectedSpecialNeeds,
-        };
-
-        try {
-            const choicesRef = ref(database, "choices");
-            const newChoiceRef = push(choicesRef);
-            set(newChoiceRef, data).then(() => {
-                console.log("Dados salvos no banco de dados com sucesso!");
-            });
-        } catch (error) {
-            console.error("Erro ao salvar os dados no banco de dados:", error);
-        }
+    const handleHabilidadesChange = (event) => {
+        props.setSelectedHabilidades(event.target.value);
     };
 
     return (
         <div className="conteudo-aplicado-container">
             <SelectField
-                label="Idade: Qual é a faixa etária dos jogadores?"
-                value={selectedAgeRange}
-                onChange={handleAgeRangeChange}
-                options={[
-                    { label: "16-20 anos", value: "16-20 anos" },
-                    { label: "21-25 anos", value: "21-25 anos" },
-                    { label: "26-30 anos", value: "26-30 anos" },
-                    { label: "31+ anos", value: "31+ anos" },
-                ]}
+                label="Estilos de aprendizagem:?"
+                value={props.selectedEstiloAprendizagem}
+                onChange={handleEstiloAprendizagemChange}
+                options={props.estiloaprendizagem }
             />
 
             <SelectField
-                label="Experiência prévia: Os jogadores já têm experiência anterior com jogos ou gamificação?"
-                value={selectedExperience}
-                onChange={handleExperienceChange}
-                options={[
-                    { label: "Sim", value: "Sim" },
-                    { label: "Não", value: "Não" },
-                ]}
+                label="Interesses?"
+                value={props.selectedInteresses}
+                onChange={handleInteressesChange}
+                options={props.interesses} 
             />
 
             <SelectField
-                label="Objetivos: Quais são os objetivos dos jogadores ao participar da gamificação?"
-                value={selectedObjectives}
-                onChange={handleObjectivesChange}
-                options={[
-                    { label: "Aprender novas habilidades", value: "Aprender novas habilidades" },
-                    { label: "Melhorar habilidades existentes", value: "Melhorar habilidades existentes" },
-                    { label: "Motivação/engajamento", value: "Motivação/engajamento" },
-                    { label: "Trabalho em equipe", value: "Trabalho em equipe" },
-                    { label: "Competição", value: "Competição" },
-                    { label: "Outro", value: "Outro" },
-                ]}
+                label="Habilidades?"
+                value={props.selectedHabilidades}
+                onChange={handleHabilidadesChange}
+                options={props.habilidades}
             />
 
-            <SelectField
-                label="Motivação: O que motiva os jogadores a participarem da gamificação?"
-                value={selectedMotivation}
-                onChange={handleMotivationChange}
-                options={[
-                    { label: "Recompensas/prêmios", value: "Recompensas/prêmios" },
-                    { label: "Reconhecimento/reputação", value: "Reconhecimento/reputação" },
-                    { label: "Desafios", value: "Desafios" },
-                    { label: "Divertimento/entretenimento", value: "Divertimento/entretenimento" },
-                    { label: "Aprendizado", value: "Aprendizado" },
-                    { label: "Outro", value: "Outro" },
-                ]}
-            />
-
-            <SelectField
-                label="Estilo de aprendizado: Como os jogadores preferem aprender?"
-                value={selectedLearningStyle}
-                onChange={handleLearningStyleChange}
-                options={[
-                    { label: "Visual (através de imagens e gráficos)", value: "Visual (através de imagens e gráficos)" },
-                    { label: "Auditivo (através de áudio e explicações verbais)", value: "Auditivo (através de áudio e explicações verbais)" },
-                    { label: "Cinestésico (através de atividades práticas e experiências)", value: "Cinestésico (através de atividades práticas e experiências)" },
-                    { label: "Leitura/escrita (através de leitura e anotações)", value: "Leitura/escrita (através de leitura e anotações)" },
-                    { label: "Outro", value: "Outro" },
-                ]}
-            />
-
-            <SelectField
-                label="Necessidades especiais: Os jogadores possuem necessidades especiais que devem ser consideradas?"
-                value={selectedSpecialNeeds}
-                onChange={handleSpecialNeedsChange}
-                options={[
-                    { label: "Sim", value: "Sim" },
-                    { label: "Não", value: "Não" },
-                ]}
-            />
-
-            <button onClick={saveChoicesToDatabase}>Salvar</button>
         </div>
     );
 };
